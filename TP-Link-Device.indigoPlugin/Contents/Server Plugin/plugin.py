d5a591d33d7c4d79266b5e5d229f868a6dbd790b
berkinet <GitHub@perlman.com>
2019-08-22 16:42:56 +0200

    added polling and validation
    Picks up device info on Ui Validation and also verifies IP address is reachable. Polls every 10 seconds for device changes.

--- TP-Link-Device.indigoPlugin/Contents/Server Plugin/plugin.py
+++ TP-Link-Device.indigoPlugin/Contents/Server Plugin/plugin.py
@@ -5,11 +5,9 @@
 # http://www.indigodomo.com
 
 import indigo
-
 import os
 import sys
 import json
-
 from tplink_smartplug import tplink_smartplug
 
 # Note the "indigo" module is automatically imported and made available inside
@@ -30,8 +28,99 @@ class Plugin(indigo.PluginBase):
 		self.logger.debug(u"shutdown called")
 
 	########################################
+	# Poll all of the states from the smart plug and pass values to
+	# Indigo Server.
+	def _refreshStatesFromHardware(self, dev, logRefresh):
+		# self.logger.info(u"Got Here 1 with %s" % (dev))
+		addr = dev.address
+		port = 9999
+		self.logger.info("TPlink name={}, addr={}".format(dev.name, addr))
+		tplink_dev = tplink_smartplug (addr, port)
+		
+		result = tplink_dev.send('info')
+		# indigo.server.log("Received sRcvd: |%s|" % (binascii.hexlify(bytearray(sRcvd))), type="TP-Link", isError=True)
+		data = json.loads(result)
+		state = data['system']['get_sysinfo']['relay_state']
+		dev.updateStateOnServer("onOffState", state)
+
+		if "CurWatts" in dev.states:
+			result = tplink_dev.send('energy')
+			data = json.loads(result)
+			indigo.server.log("Received result: |%s|" % (result), type="TP-Link", isError=True)
+			curWatts = data['emeter']['get_realtime']['power_mw']
+			curVolts = data['emeter']['get_realtime']['voltage_mv']
+			curAmps  = data['emeter']['get_realtime']['current_ma']
+
+			dev.updateStateOnServer("CurWatts", curWatts)
+			dev.updateStateOnServer("CurVolts", curVolts)
+			dev.updateStateOnServer("CurAmps", curAmps)
+
+			indigo.server.log("Received results: %s, %s, %s" % (curWatts, curVolts, curAmps), type="TP-Link", isError=True)
+
+		#indigo.server.log(u"Update received for %s: state %s, watts %s" % (dev.name, state, curWatts), type="TP-Link", isError=False)
+
+	########################################
+	def runConcurrentThread(self):
+		try:
+			while True:
+				for dev in indigo.devices.iter("self"):
+					if not dev.enabled or not dev.configured:
+						continue
+
+					# Plugins that need to poll out the status from the meter
+					# could do so here, then broadcast back the new values to the
+					# Indigo Server.
+					self._refreshStatesFromHardware(dev, False)
+
+				self.sleep(10)
+		except self.StopThread:
+			pass	# Optionally catch the StopThread exception and do any needed cleanup.
+			
+	########################################
 	def validateDeviceConfigUi(self, valuesDict, typeId, devId):
-		return (True, valuesDict)
+		errorsDict = indigo.Dict()
+
+		self.logger.info(u"received \"%s\"" % (valuesDict))
+		cmd = "/sbin/ping -c1 -t5 -q " + valuesDict['address'] + " >/dev/null 2>&1" 
+		response = os.system(cmd)
+		# indigo.server.log("Response: %s " % (response), type="TP-Link", isError=True)
+		
+		#and then check the response...
+		if int(response) != 0:
+			self.logger.info(u"%s is not reachable" % valuesDict['address'], isError=True)
+			errorsDict["address"] = "Host unreachable"
+			return (False, valuesDict, errorsDict)
+		
+		addr = valuesDict['address']
+		port = 9999
+		
+		tplink_dev = tplink_smartplug (addr, port)
+		result = tplink_dev.send('info')
+		#if logRefresh:
+		# self.logger.info(u"received \"%s\" %s to %s" % (dev.name, "power load", result))
+
+		data = json.loads(result)
+		valuesDict["Name"] = data['system']['get_sysinfo']['dev_name']
+		valuesDict["Alias"] = data['system']['get_sysinfo']['alias']
+		valuesDict["Model"]  = data['system']['get_sysinfo']['model']
+		valuesDict["MAC"]  = data['system']['get_sysinfo']['mac']
+		valuesDict["configured"]  = "true"
+
+		return (True, valuesDict, errorsDict)
+
+	########################################
+	def deviceStartComm(self, dev):
+		# Called when communication with the hardware should be established.
+		# Here would be a good place to poll out the current states from the
+		# meter. If periodic polling of the meter is needed (that is, it
+		# doesn't broadcast changes back to the plugin somehow), then consider
+		# adding that to runConcurrentThread() above.
+		self._refreshStatesFromHardware(dev, True)
+		pass
+
+	def deviceStopComm(self, dev):
+		# Called when communication with the hardware should be shutdown.
+		pass
 
 	########################################
 	# Relay / Dimmer Action callback
